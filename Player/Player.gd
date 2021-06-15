b18a6018bdf5851719f757bee031b5749887dd6b
IAmCheeseman <64710123+IAmCheeseman@users.noreply.github.com>
2021-06-15 18:11:18 -0400

    Added basic movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
 
+onready var sprite = $Sprite
+onready var anim = $AnimationPlayer
+
 export var speed = 90
 
 var velocity = Vector2.ZERO
@@ -8,10 +11,25 @@ var isDead = false
 
 
 func _physics_process(delta):
+	movement()
+	face_mouse()
+
+	velocity = move_and_slide(velocity*delta)
+
+
+func movement():
 	# Movement
 	velocity = Vector2.ZERO
 	velocity.y = Input.get_action_strength("S")-Input.get_action_strength("W")
 	velocity.x = Input.get_action_strength("D")-Input.get_action_strength("A")
 	velocity = velocity.normalized()*speed
 
-	velocity = move_and_slide(velocity)
+	if velocity.is_equal_approx(Vector2.ZERO): anim.play("Idle")
+	else: anim.play("Run")
+
+
+func face_mouse():
+	var mousePos = get_local_mouse_position()
+	var spriteScale = -round(mousePos.normalized().x)
+
+	sprite.scale.x = spriteScale if spriteScale != 0 else sprite.scale.x
